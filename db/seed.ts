9342a3b857ff3817a5e725db19ced90e509398a5
Sujan  Timalsina <iamsjn44@gmail.com>
2026-08-15 18:36:47 +0545

    Removed application line on seed file

--- db/seed.ts
+++ db/seed.ts
@@ -5,9 +5,8 @@ import { prisma } from "@/db/prisma";
 import sampleData from "./sample-data";
 
 async function main() {
-  await prisma.application.deleteMany();
-
-  await prisma.application.createMany({ data: sampleData.applications });
+  // await prisma.application.deleteMany();
+  // await prisma.application.createMany({ data: sampleData.applications });
 }
 
 main();
